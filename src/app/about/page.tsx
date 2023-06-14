c6ba2fbe32027b671b95dba0818c3c976bc34fbf
John Cox <john.cox0321@gmail.com>
2023-06-13 21:48:59 -0500

    fix(All): center NebulaEffect on all pages.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,9 +4,12 @@ import NormalNavbar from '@/components/NormalNavbar'
 export default function About() {
   return (
     <main className="cursor-custom bg-stars bg-cover min-h-screen">
-      <NormalNavbar />
-      <div className="flex items-center justify-center min-h-screen">
-        <NebulaEffect />
+      <NebulaEffect />
+      <div className='absolute w-full'>
+        <NormalNavbar />
+        <div>
+          <p className='text-white font-michroma text-3xl'>About Me</p>
+        </div>
       </div>
     </main>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,18 @@ import FooterIcon from '@/components/FooterIcon'
 
 export default function Home() {
   return (
-    <main className="cursor-custom bg-stars bg-cover flex items-center justify-center min-h-screen">
+    <main className="cursor-custom bg-stars bg-cover min-h-screen">
       <NebulaEffect />  
-      <div className='absolute'>  
-        <div className='flex justify-center'>
-          <Image src="./Willow.svg" width="75" height="75" alt="Willow the Planet Logo"/>
-          <p className='animate-animate-text bg-gradient-to-r from-dark-babyblue via-babyblue via-blurple via-purple via-blurple via-babyblue to-dark-babyblue font-michroma bg-clip-text text-white text-4xl sm:text-5xl hover:text-transparent'>John Cox</p>
-        </div>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <div className='absolute'>
+          <div className='flex justify-center'>
+            <Image src="./Willow.svg" width="75" height="75" alt="Willow the Planet Logo"/>
+            <p className='animate-animate-text bg-gradient-to-r from-dark-babyblue via-babyblue via-blurple via-purple via-blurple via-babyblue to-dark-babyblue font-michroma bg-clip-text text-white text-4xl sm:text-5xl hover:text-transparent'>John Cox</p>
+          </div>
           <HomeNavbar />
           <FooterIcon /> 
         </div>
+      </div>
     </main>
   )
 } 
