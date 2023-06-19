7902778d3bd17dc1304902ff63e7a8ee01df0f7c
John Cox <john.cox0321@gmail.com>
2023-06-18 19:33:17 -0500

    feat(about): add underline to experience titles

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,6 +2,21 @@ import FooterIcon from '@/components/FooterIcon'
 import NebulaEffect from '@/components/NebulaEffect'
 import NormalNavbar from '@/components/NormalNavbar'
 import Link from 'next/link'
+import { projects } from '@/components/Projects'
+
+interface ProjectCardProps {
+  key: number;
+  children: React.ReactNode;
+}
+
+const ProjectCard = (props: ProjectCardProps) => {
+  return (
+    <li className='rounded-lg bg-gradient-to-b from-purple via-transparent to-dark-babyblue hover:scale-105'>
+      {props.children}
+    </li>
+  )
+
+}
 
 export default function Projects() {
   return (
@@ -13,70 +28,29 @@ export default function Projects() {
           <p className='flex items-center justify-center text-white font-michroma text-4xl underline underline-offset-8 pt-16'>PROJECTS</p>
           <div className='justify-center'>
             <ul className='mt-16 grid grid-cols-1 lg:grid-cols-3 gap-4'>
-            <li className='rounded-lg bg-gradient-to-b from-purple via-transparent to-dark-babyblue hover:scale-105'>
-                <Link href={"/"}>
-                  <div className='flex flex-col justify-center items-center mx-14 my-8'>
-                    <span className='font-exo text-white text-lg font-semibold'>Website Portfolio</span>
-                    <span className='font-exo text-white text-lg font-extralight'>June 23'</span>
-                    <div className='flex mt-1'>
-                      <img src="./project_icon/nextjs.svg" width="25" height="25" alt="Nextjs" className='mx-1' />
-                      <img src="./project_icon/tailwind.svg" width="25" height="25" alt="TailwindCSS" className='mx-1'/>
-                      <img src="./project_icon/typescript.svg" width="25" height="25" alt="TypeScript" className='mx-1'/>
-                      <img src="./project_icon/Figma.svg" width="25" height="25" alt="Figma" className='mx-1'/>
-                    </div>
-                  </div>
-                </Link>
-              </li>
-              <li className='rounded-lg bg-gradient-to-b from-purple via-transparent to-dark-babyblue hover:scale-105'>
-                <Link href={"/"}>
-                  <div className='flex flex-col justify-center items-center mx-14 my-8'>
-                    <span className='font-exo text-white text-lg font-semibold'>Theme Park Website</span>
-                    <span className='font-exo text-white text-lg font-extralight'>April 23'</span>
-                    <div className='flex mt-1'>
-                      <img src="./project_icon/mysql.svg" width="25" height="25" alt="MySQL" className='mx-1' />
-                      <img src="./project_icon/nodejs.svg" width="25" height="25" alt="Nodejs" className='mx-1'/>
-                      <img src="./project_icon/javascript.svg" width="25" height="25" alt="Javascript" className='mx-1'/>
-                    </div>
-                  </div>
-                </Link>
-              </li>
-              <li className='rounded-lg bg-gradient-to-b from-purple via-transparent to-dark-babyblue hover:scale-105'>
-                <Link href={"/"} className='cursor-custom'>
-                  <div className='flex flex-col justify-center items-center mx-14 my-8'>
-                    <span className='font-exo text-white text-lg font-semibold'>Hekima RPG</span>
-                    <span className='font-exo text-white text-lg font-extralight'>Jan 23'</span>
-                    <div className='flex mt-1'>
-                      <img src="./project_icon/sequelize.svg" width="25" height="25" alt="Sequelize" className='mx-1' />
-                      <img src="./project_icon/javascript.svg" width="25" height="25" alt="Javascript" className='mx-1'/>
-                      <img src="./project_icon/googlecloud.svg" width="25" height="25" alt="Google Cloud" className='mx-1'/>
-                      <img src="./project_icon/discord.svg" width="25" height="25" alt="DiscordJS" className='mx-1'/>
-                    </div>
-                  </div>
-                </Link>
-              </li>
-              <li className='rounded-lg bg-gradient-to-b from-purple via-transparent to-dark-babyblue hover:scale-105'>
-                <Link href={"/"}>
-                  <div className='flex flex-col justify-center items-center mx-14 my-8'>
-                    <span className='font-exo text-white text-lg font-semibold'>Alone</span>
-                    <span className='font-exo text-white text-lg font-extralight'>July 22'</span>
-                    <div className='flex mt-1'>
-                      <img src="./project_icon/python.svg" width="25" height="25" alt="Python" className='mx-1' />
-                    </div>
-                  </div>
-                </Link>
-              </li>
-              <li className='rounded-lg bg-gradient-to-b from-purple via-transparent to-dark-babyblue hover:scale-105'>
-                <Link href={"/"}>
-                  <div className='flex flex-col justify-center items-center mx-14 my-8'>
-                    <span className='font-exo text-white text-lg font-semibold'>Recent Logins</span>
-                    <span className='font-exo text-white text-lg font-extralight'>March 21'</span>
-                    <div className='flex mt-1'>
-                      <img src="./project_icon/opencv.svg" width="25" height="25" alt="OpenCV" className='mx-1' />
-                      <img src="./project_icon/python.svg" width="25" height="25" alt="Python" className='mx-1'/>
-                    </div>
-                  </div>
-                </Link>
-              </li>
+              {
+                projects.map((project, ind) => {
+                  return (
+                    <ProjectCard key={ind}>
+                      <Link href={project.url} target="_blank" className='cursor-custom'>
+                        <div className='flex flex-col justify-center items-center mx-14 my-8'>
+                          <span className='font-exo text-white text-lg font-semibold'>{project.name}</span>
+                          <span className='font-exo text-white text-lg font-extralight'>{project.date_completed}</span>
+                            <div className='flex mt-1'>
+                              {
+                                project.icons.map((icon, icon_ind) => {
+                                  return (
+                                    <img src={`./project_icon/${icon}.svg`} width="25" height="25" alt="Nextjs" className='mx-1' />
+                                  )
+                                })
+                              }
+                            </div>
+                        </div>
+                      </Link>
+                    </ProjectCard>
+                  )
+                })
+              }
             </ul>
           </div>
             <FooterIcon />
