8961522c16b6037805a0848028e6f47c51f37d09
programwithjohn <john.cox0321@gmail.com>
2023-06-22 00:27:11 -0500

    fix(entry): add in dev message

--- src/app/entry/page.tsx
+++ src/app/entry/page.tsx
@@ -5,9 +5,8 @@ export default function Entry1() {
     <main className="cursor-custom bg-stars bg-cover min-h-screen">
       <div className="flex flex-col items-center justify-center min-h-screen">
         <div className='absolute'>
-          <div className='flex justify-center'>
-            <Image src="./Willow.svg" width="75" height="75" alt="Willow the Planet Logo" className='hover:animate-bounce'/>
-            <p className='animate-animate-text bg-gradient-to-r from-dark-babyblue via-babyblue via-blurple via-purple via-blurple via-babyblue to-dark-babyblue font-michroma bg-clip-text text-white text-4xl sm:text-5xl hover:text-transparent'>Entry 1</p>
+        <div className='flex justify-center'>
+            <p className='animate-animate-text bg-gradient-to-r from-dark-babyblue via-babyblue via-blurple via-purple via-blurple via-babyblue to-dark-babyblue font-michroma bg-clip-text text-white text-4xl sm:text-5xl hover:text-transparent'>In development</p>
           </div>
         </div>
       </div>
