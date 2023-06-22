f1ffcde99339ac7e76efec11c1ea73fa5636b393
programwithjohn <john.cox0321@gmail.com>
2023-06-21 21:24:07 -0500

    added a lot, forgot to commit

--- src/app/astrophotography/page.tsx
+++ src/app/astrophotography/page.tsx
@@ -2,69 +2,53 @@ import FooterIcon from '@/components/FooterIcon'
 import NebulaEffect from '@/components/NebulaEffect'
 import NormalNavbar from '@/components/NormalNavbar'
 import ImageSlider from '@/components/ImageSlider'
-
 import Link from 'next/link'
 
-interface DateCardProps {
-  key: number;
-  children: React.ReactNode;
-}
-
-const DateCard = (props: DateCardProps) => {
-  return (
-    <li className='rounded-lg bg-gradient-to-b from-purple via-transparent to-dark-babyblue hover:scale-105'>
-      {props.children}
-    </li>
-  )
-
-}
-
-    const img = [
-        {
-            id: "1",
-            src: "./3-26-23/img1.png",
-            alt: "image 1"
-        },
-        {
-            id: "2",
-            src: "./3-26-23/img2.png",
-            alt: "image 2"
-        },
-        {
-            id: "3",
-            src: "./3-26-23/img3.png",
-            alt: "image 3"
-        },
-        {
-            id: "4",
-            src: "./3-26-23/img4.png",
-            alt: "image 4"
-        },
-        {
-            id: "5",
-            src: "./3-26-23/img5.png",
-            alt: "image 5"
-        },
-        {
-            id: "6",
-            src: "./3-26-23/img6.png",
-            alt: "image 6"
-        },
-        {
-            id: "8",
-            src: "./3-26-23/img8.png",
-            alt: "image 8"
-        },
-        {
-            id: "8",
-            src: "./3-26-23/img8.png",
-            alt: "image 8"
-        },
-    ]
+const img = [
+    {
+        id: "1",
+        src: "./3-26-23/img1.png",
+        alt: "image 1"
+    },
+    {
+        id: "2",
+        src: "./3-26-23/img2.png",
+        alt: "image 2"
+    },
+    {
+        id: "3",
+        src: "./3-26-23/img3.png",
+        alt: "image 3"
+    },
+    {
+        id: "4",
+        src: "./3-26-23/img4.png",
+        alt: "image 4"
+    },
+    {
+        id: "5",
+        src: "./3-26-23/img5.png",
+        alt: "image 5"
+    },
+    {
+        id: "6",
+        src: "./3-26-23/img6.png",
+        alt: "image 6"
+    },
+    {
+        id: "8",
+        src: "./3-26-23/img8.png",
+        alt: "image 8"
+    },
+    {
+        id: "8",
+        src: "./3-26-23/img8.png",
+        alt: "image 8"
+    },
+]
 
 export default function Astrophotography() {
     return (
-        
         <main className="cursor-custom bg-stars bg-cover h-screen">
             <NebulaEffect />
             <div className='absolute w-full'>
@@ -74,11 +58,43 @@ export default function Astrophotography() {
                     <div className='mt-10'>
                         <ImageSlider images={img} />
                     </div>
-                    <div>
-                        <ul className='grid grid-cols-1 lg:grid-cols-3 gap-4'>
-                          <Link>
-                            <span className='font-exo text-white text-lg font-semibold'>3-26-23</span>
-                          </Link>
+                    <div className='md:mt-6'>
+                        <ul className='grid grid-cols-2 lg:grid-cols-3 gap-4'>
+                          <li className='rounded-lg bg-gradient-to-b from-purple via-transparent to-dark-babyblue hover:scale-105 transition-all duration-500 ease-out'>
+                            <div className='flex flex-col justify-center items-center mx-8 my-4 md:mx-10 md:my-6'>
+                              <Link href={"/entry"}>
+                                <span className='font-exo text-white text-lg font-semibold'>3.26.23</span>
+                              </Link>
+                            </div>
+                          </li>
+                          <li className='rounded-lg bg-gradient-to-b from-purple via-transparent to-dark-babyblue hover:scale-105 transition-all duration-500 ease-out'>
+                            <div className='flex flex-col justify-center items-center md:mx-10 md:my-6'>
+                              <Link href={"/entry"}>
+                                <span className='font-exo text-white text-lg font-semibold'>3-26-23</span>
+                              </Link>
+                            </div>
+                          </li>
+                          <li className='rounded-lg bg-gradient-to-b from-purple via-transparent to-dark-babyblue hover:scale-105 transition-all duration-500 ease-out'>
+                            <div className='flex flex-col justify-center items-center md:mx-10 md:my-6'>
+                              <Link href={"/entry"}>
+                                <span className='font-exo text-white text-lg font-semibold'>3-26-23</span>
+                              </Link>
+                            </div>
+                          </li>
+                          <li className='rounded-lg bg-gradient-to-b from-purple via-transparent to-dark-babyblue hover:scale-105 transition-all duration-500 ease-out'>
+                            <div className='flex flex-col justify-center items-center md:mx-10 md:my-6'>
+                              <Link href={"/entry"}>
+                                <span className='font-exo text-white text-lg font-semibold'>3-26-23</span>
+                              </Link>
+                            </div>
+                          </li>
+                          <li className='rounded-lg bg-gradient-to-b from-purple via-transparent to-dark-babyblue hover:scale-105 transition-all duration-500 ease-out'>
+                            <div className='flex flex-col justify-center items-center md:mx-10 md:my-6'>
+                              <Link href={"/entry"}>
+                                <span className='font-exo text-white text-lg font-semibold'>3-26-23</span>
+                              </Link>
+                            </div>
+                          </li>
                         </ul>
                     </div>
                     <FooterIcon />
--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -14,13 +14,6 @@ interface CarouselProps {
   images: ImageData[];
 }
 
-/**
- * Carousel component for nextJS and Tailwind.
- * Using external library react-easy-swipe for swipe gestures on mobile devices (optional)
- *
- * @param images - Array of images with src and alt attributes
- * @returns React component
- */
 export default function ImageSlider({ images }: CarouselProps) {
   const [currentSlide, setCurrentSlide] = useState<number>(0);
 
@@ -35,93 +28,38 @@ export default function ImageSlider({ images }: CarouselProps) {
   };
 
   return (
-    <div className="flex flex-col">
-      <div className="flex flex-row">
-        <AiOutlineLeft
-          onClick={handlePrevSlide}
-          className="justify-center items-center cursor-custom text-4xl text-dark-babyblue"
-        />
+    <div className="flex flex-row justify-center items-center">
+      <AiOutlineLeft
+        onClick={handlePrevSlide}
+        className="cursor-custom text-5xl text-dark-babyblue mr-4"
+      />
 
-        <div className="w-40 h-60">
-          <Swipe
-            onSwipeLeft={handleNextSlide}
-            onSwipeRight={handlePrevSlide}
-            className=""
-            // className="z-10 w-full h-full lg:w-56 lg:h-96"
-          >
-            {images.map((image, index) => {
-              if (index === currentSlide) {
-                return (
-                  <a href={image.src} target="_blank" >
-                      <img
-                        key={image.id}
-                        src={image.src}
-                        alt={image.alt}
-                        className="animate-fadeIn"
-                      />
-                  </a>
-                );
-              }
-              return null;
-            })}
-          </Swipe>  
-        </div>
-
-
-        <AiOutlineRight
-          onClick={handleNextSlide}
-          className="justify-center items-center cursor-custom text-4xl text-dark-babyblue"
-        />
+      <div className="w-40 h-64 md:w-72 md:h-96">
+        <Swipe
+          onSwipeLeft={handleNextSlide}
+          onSwipeRight={handlePrevSlide}
+        >
+          {images.map((image, index) => {
+            if (index === currentSlide) {
+              return (
+                <a href={image.src} target="_blank" >
+                    <img
+                      key={image.id}
+                      src={image.src}
+                      alt={image.alt}
+                      className="animate-fadeIn"
+                    />
+                </a>
+              );
+            }
+            return null;
+          })}
+        </Swipe>  
       </div>
+      <AiOutlineRight
+        onClick={handleNextSlide}
+        className="cursor-custom text-5xl text-dark-babyblue ml-4"
+      />
     </div>
   );
 }
-
-
-
-// {/* <div className="w-full h-[50vh] overflow-hidden scale-50">
-//         {/* <div className="w-full h-[50vh] flex overflow-hidden relative m-auto"> */}
-//           <Swipe
-//             onSwipeLeft={handleNextSlide}
-//             onSwipeRight={handlePrevSlide}
-//             className=""
-//             // className="z-10 w-full h-full lg:w-56 lg:h-96"
-//           >  
-//             {images.map((image, index) => {
-//               if (index === currentSlide) {
-//                 return (
-//                   <a href={image.src} target="_blank" >
-//                       <img
-//                         key={image.id}
-//                         src={image.src}
-//                         alt={image.alt}
-//                         className="animate-fadeIn"
-//                       />
-//                   </a>
-//                 );
-//               }
-//               return null;
-//             })}
-//           </Swipe>
-//         </div>
-//       </div>
-
-//       <div className="">
-//       {/* <div className="relative flex justify-center p-2"> */}
-//         {images.map((_, index) => {
-//           return (
-//             <div
-//               className={
-//                 index === currentSlide
-//                   ? "h-4 w-4 bg-gray-700 rounded-full mx-2 mb-2 cursor-pointer"
-//                   : "h-4 w-4 bg-gray-300 rounded-full mx-2 mb-2 cursor-pointer"
-//               }
-//               key={index}
-//               onClick={() => {
-//                 setCurrentSlide(index);
-//               }}
-//             />
-//           );
-//         })}
-//       </div>
-//     </div> */}
