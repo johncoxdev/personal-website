ccfb575f79499cd0e4cf4d9f813fc244fcc7df87
programwithjohn <john.cox0321@gmail.com>
2023-06-22 00:30:02 -0500

    fix(entry): add in dev message

--- src/app/entry/page.tsx
+++ src/app/entry/page.tsx
@@ -6,7 +6,7 @@ export default function Entry1() {
       <div className="flex flex-col items-center justify-center min-h-screen">
         <div className='absolute'>
         <div className='flex justify-center'>
-            <p className='animate-animate-text bg-gradient-to-r from-dark-babyblue via-babyblue via-blurple via-purple via-blurple via-babyblue to-dark-babyblue font-michroma bg-clip-text text-white text-4xl sm:text-5xl hover:text-transparent'>In development</p>
+            <p className='animate-animate-text bg-gradient-to-r from-dark-babyblue via-babyblue via-blurple via-purple via-blurple via-babyblue to-dark-babyblue font-michroma bg-clip-text text-white text-4xl sm:text-5xl hover:text-transparent'>In development!</p>
           </div>
         </div>
       </div>
