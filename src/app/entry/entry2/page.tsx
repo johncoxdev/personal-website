4e6f163c5706e39773121cfdfc774a8db0278cf7
programwithjohn <john.cox0321@gmail.com>
2023-06-22 00:23:21 -0500

    feat(entry): add entry subfolder for pages

--- src/app/entry/entry2/page.tsx
+++ src/app/entry/entry2/page.tsx
@@ -6,8 +6,7 @@ export default function Entry2() {
       <div className="flex flex-col items-center justify-center min-h-screen">
         <div className='absolute'>
           <div className='flex justify-center'>
-            <Image src="./Willow.svg" width="75" height="75" alt="Willow the Planet Logo" className='hover:animate-bounce'/>
-            <p className='animate-animate-text bg-gradient-to-r from-dark-babyblue via-babyblue via-blurple via-purple via-blurple via-babyblue to-dark-babyblue font-michroma bg-clip-text text-white text-4xl sm:text-5xl hover:text-transparent'>Entry 2</p>
+            <p className='animate-animate-text bg-gradient-to-r from-dark-babyblue via-babyblue via-blurple via-purple via-blurple via-babyblue to-dark-babyblue font-michroma bg-clip-text text-white text-4xl sm:text-5xl hover:text-transparent'>In development</p>
           </div>
         </div>
       </div>
