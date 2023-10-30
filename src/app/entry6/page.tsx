40423ba0954dd4d54c94999ac302ae49c1ee9da6
John Cox <john.cox0321@gmail.com>
2023-10-30 15:06:34 -0500

    update(resume): add new resume

--- src/app/entry6/page.tsx
+++ src/app/entry6/page.tsx
@@ -20,7 +20,7 @@ export default function Entry5() {
                     <div className='flex flex-col mt-3 w-11/12 sm:w-4/12'>
                       <p className='font-exo text-white'>
                         The school year was over, after being stressed out for weeks over finals. It was finally over, and I had so much time on my hands to look at the sky and do what I loved once again. I only looked at the moon this night, and I remember loving every second of it once again. This time, I had a new thing I wanted to tackle. Editing the photos just as every other astrophotographer does. In the last image, the slightly more orange moon, is the only image I edited to attempt to make it “better”. Although I loved the image originally, the newly edited image was something new to love. <br></br>
-                        My mind was still on March 25th and wanting to look at stars, constellations, nebulas, and planets. Ever since March 25th I set my goal to continue to dive into the career I want to do (Data Science) and continue with my loving hobby of astronomy/astrophotography. Although I may be an amateur one, Ill always continue to get better equipment to bring the beauty of space to my screen and others. 
+                        My mind was still on March 25th and wanting to look at stars, constellations, nebulas, and planets. Ever since March 25th, I set my goal to continue to dive into the career I want to do (Data Science) and continue with my loving hobby of astronomy/astrophotography. Although I may be an amateur one, Ill always continue to get better equipment to bring the beauty of space to my screen and others. 
                       </p>
                     </div>
                     <FooterIcon />
